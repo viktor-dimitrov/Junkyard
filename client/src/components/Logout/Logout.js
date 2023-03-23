9b20f8e9322a6d97e5e5e1c7a512d713c674f904
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-03-23 14:52:14 +0200

    move files in client folder

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -1,14 +1,8 @@
 import { useContext } from "react";
-import { Navigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthContext";
 
 
 export default function Logout () {
-
     const { onLogout } = useContext(AuthContext);
-
     onLogout();
-
-
-
 }
