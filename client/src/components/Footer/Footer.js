8a2cf073da53a52185c0ade932410c5719cf3949
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-10-25 21:21:02 +0300

    footer update

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="wrapper">
-            <p>Copyright &copy 2013 Source X by Youssef Nassim and Zakariya Lhamri, design from <a href="http://geeksband.com">Geeksband.com</a>. All Rights Reserved.</p>
+             <strong>© 2023 Viktor Dimitrov</strong>
         </footer>
     )
 }
